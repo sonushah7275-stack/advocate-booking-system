538708bcd9f1fbb0308e2173ba103d2547bb99b2
sonushah7275-stack <sonushah7275@gmail.com>
2026-04-28 09:26:07 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,31 +1,3 @@
-// import React from "react";
-// import Login from "./pages/Login";
-// import { ToastContainer, toast } from "react-toastify";
-// import "react-toastify/dist/ReactToastify.css";
-// import { AdminContext } from "./context/AdminContext";
-// import Navbar from "./components/Navbar";
-// import Sidebar from "./components/Sidebar";
-// const App = () => {
-//   const { aToken } = useContext(AdminContext);
-
-//   return aToken ? (
-//     <div className="bg-[#F8F9FD]">
-//       <ToastContainer />
-//       <Navbar />
-//       <div>
-//         <Sidebar />
-//       </div>
-//     </div>
-//   ) : (
-//     <>
-//       <Login />
-//       <ToastContainer />
-//     </>
-//   );
-// };
-
-// export default App;
-
 import React, { useContext } from "react";
 import Login from "./pages/Login";
 import { ToastContainer, toast } from "react-toastify";
