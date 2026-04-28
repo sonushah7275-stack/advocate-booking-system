353e6d9a7717566d471f044ca6f64db7829e97f1
sonushah7275-stack <sonushah7275@gmail.com>
2026-04-28 09:23:10 +0530

    Update AdminContext.jsx

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -6,7 +6,7 @@ export const AdminContext = createContext();
 
 const AdminContextProvider = ({ children }) => {
   const backendUrl =
-    import.meta.env.VITE_BACKEND_URL || "http://localhost:4000";
+    import.meta.env.VITE_BACKEND_URL || "https://advocate-booking-system-backend.onrender.com";
 
   const [aToken, setAToken] = useState(
     localStorage.getItem("aToken") ? localStorage.getItem("aToken") : "",
